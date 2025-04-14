3b410f2c1b3cd63b35f94ab30a673511c8469893
VijaykumarYavagal <118459440+VijaykumarYavagal@users.noreply.github.com>
2025-04-14 22:22:05 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,29 +1,28 @@
-import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server';
 
 const isProtectedRoute = createRouteMatcher([
-"/dashboard(.*)",
+  "/dashboard(.*)",
   "/resume(.*)",
   "/interview(.*)",
   "/ai-cover-letter(.*)",
   "/onboarding(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
-   const {userId}= await auth()
+export default clerkMiddleware((auth, req) => {
+  const { userId, redirectToSignIn } = auth;
 
-   if(!userId && isProtectedRoute(req)){
-    const {redirectToSignIn} = await auth()
-    return redirectToSignIn()
- }
-   return NextResponse.next();
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn();
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
